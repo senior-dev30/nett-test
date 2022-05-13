5318b8a48ce270127595056a7b26b428cbc42f35
yamamoto-ichiro <yamamoto.ichiro92@gmail.com>
2022-05-13 07:01:51 +0100

    Install storybook with vite-builder

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+const { mergeConfig } = require('vite');
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  async viteFinal(config) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: { foo: 'bar' },
+      },
+    });
+  },
+};
